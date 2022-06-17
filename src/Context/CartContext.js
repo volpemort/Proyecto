034f678658f3ca18e3f7bb8ec33818482b71883a
Julian Volpe <julian.volpemort@gmail.com>
2022-06-17 16:38:20 -0300

    Cart

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -5,11 +5,12 @@ const CartContext = createContext ()
 const CartProvider = ({children}) =>{
 
     const [cartListItems, setCartListItems] = useState([])
-
+    const [totalPrice, setTotalPrice] = useState(0)
     const addProductToCart = (product) =>{
         let isInCart = cartListItems.find(cartItem => cartItem.id === product.id)
         if(!isInCart) {
             console.log("se agrego el producto:", product)
+            setTotalPrice(totalPrice + product.price)
             return setCartListItems(cartListItems => [...cartListItems, product])
         }
         console.log("El producto ya se encuentra en el carrito")
@@ -21,6 +22,7 @@ const CartProvider = ({children}) =>{
 
     const data ={
         cartListItems,
+        totalPrice,
         addProductToCart,
         deleteProduct
     }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,52 @@
 
-const Cart = () =>{
-    return (
-            <div>
-                <p>asdas</p>
-            </div>
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Container } from '@mui/material';
+import "./Cart.css" 
+import { useState, useContext } from 'react';
+import CartContext from '../../Context/CartContext'
 
+const Cart = () =>{
+
+    const { cartListItems, deleteProduct, totalPrice  } = useContext(CartContext)
+    return (
+        <Container>
+            <div className='cartMenu'>
+                <div>
+                    <h2>Producto</h2>
+                    <h2>Descripcion</h2>
+                    <h2>Cantidad</h2>
+                    <h2>Precio</h2>
+                    <h2>Quitar</h2>
+                </div>
+            </div>
+            {cartListItems.map( (item) => {
+                        return(
+                        <div className='item-cart-prod' key={item.id}>
+                            <div className='cart-image'>
+                                <img src={`/${item.image}`} alt="prod carrito" />
+                            </div>
+                            <div className='cart-info'>
+                                <div>
+                                    <p>{item.title}</p>
+                                    <h3>1</h3>        
+                                    <span>$ {item.price}</span>
+                                </div>
+                                
+                            </div>
+                            <div className='cart-prod__action'>
+                                <button onClick={()=> deleteProduct(item.id)}>
+                                    <DeleteIcon />
+                                </button>
+                               
+                            </div>
+                        </div>
+                        )
+                    })}
+            <div>
+                <p>Precio Total</p>
+                <span>${totalPrice}</span>
+            </div>
+        </Container>
         )}
 
 export default Cart
